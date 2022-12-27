ba6ea5f49b5a7526f2657d84b6c6d90ceeb885c5
Albert <albert@framework.tools>
2022-12-27 21:31:37 +1030

    Update tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,13 +6,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="manifest" href="/manifest.json">
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-8MK9JSEJ2P"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-C6CCLY5L3K"></script>
     <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
-        gtag('config', 'G-8MK9JSEJ2P');
+    gtag('config', 'G-C6CCLY5L3K');
     </script>
 </svelte:head>
 <PageLoaderBar/>
